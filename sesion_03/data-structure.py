882ade58a60561f20b0f3e044899c8363494b691
HugoRoca <hugo.rock20@hotmail.com>
2024-05-21 23:33:58 -0500

    session 03

---
+++ sesion_03/data-structure.py
@@ -0,0 +1,16 @@
+my_list = [-1, 5.0, 10, 'ok', 'not']
+
+print(my_list)
+
+my_tuple = (-1, 5.0, 10, 'ok', 'not')
+print(my_tuple)
+
+my_dict = {'item1': 13, 'item2': 14, 'item3': 20}
+print(my_dict)
+
+my_set = {-1, 5.0, 10, 'ok', 'not'}
+print(my_set)
+freez_my_set = frozenset(my_set)
+
+# How to convert from Data Tyoe to another?
+print(type(my_set))
